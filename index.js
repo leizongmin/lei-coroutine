7ed3efb671da919d739b0473dae4eea02142f3b3
Zongmin Lei <leizongmin@gmail.com>
2016-10-19 22:32:34 +0800

    init

---
+++ index.js
@@ -0,0 +1,68 @@
+'use strict';
+
+/**
+ * lei-coroutine
+ * 
+ * @author Zongmin Lei <leizongmin@gmail.com>
+ */
+
+/**
+ * 检查是否为Promise对象
+ *
+ * @param {Object} p
+ * @return {Boolean}
+ */
+function isPromise(p) {
+  return typeof p.then === 'function' && typeof p.catch === 'function';
+}
+
+function wrapGen(genFn) {
+  return function () {
+    return new Promise((resolve, reject) => {
+      const gen = genFn.apply(null, arguments);
+      function next(value) {
+        const ret = gen.next(value);
+        // 如果done=true则表示结束
+        if (ret.done) return resolve(ret.value);
+        // 如果是promise则执行
+        if (isPromise(ret.value)) return ret.value.then(next).catch(reject);
+        // 其他值则报错
+        return reject(new TypeError(`you can only yield promise but got type ${ typeof ret.value }`));
+      }
+      // 开始执行
+      next();
+    });
+  };
+}
+
+/**
+ * coroutine包装函数
+ *
+ * @param {Function} genFn
+ * @return {Function}
+ */
+function wrap(genFn) {
+  // 包装generator函数
+  const fn = wrapGen(genFn);
+  // 保留函数名和参数信息
+  const info = genFn.toString().match(/function\s*\*\s*(.*\(.*\))/);
+  const sign = info && info[1] ? info[1] : '()';
+  const code = `(function ${ sign } { return fn.apply(this, arguments); })`;
+  return eval(code);
+}
+
+/**
+ * 执行coroutine函数
+ * 
+ * @param {Function} genFn
+ * @param {Mixed} param1
+ * @param {Mixed} param2
+ * @return {Promise}
+ */
+function exec(genFn) {
+  return wrap(genFn).apply(this, Array.prototype.slice.call(arguments, 1));
+}
+
+module.exports = exports = exec;
+exports.exec = exec;
+exports.wrap = wrap;
